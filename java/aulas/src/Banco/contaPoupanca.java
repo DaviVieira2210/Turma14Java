d3f3b55c601a06705e8446e4b26ae7dafa0a0b8e
DaviVieira2210 <davi.vieira205@gmail.com>
2020-12-12 23:17:14 -0300

    Conta Poupança

--- java/aulas/src/Banco/contaPoupanca.java
+++ java/aulas/src/Banco/contaPoupanca.java
@@ -11,6 +11,8 @@ public class contaPoupanca {
 		
 		double movimentos[] = new double[3];
 		
+		int aniversarioConfirmacao=0;
+		int aniversarioConta = 22;
 		char debitoCredito = 0;
 		char continuaLoop = 0;
 		double credito=0, debito = 0;
@@ -19,12 +21,16 @@ public class contaPoupanca {
 		String nomeClientes = "Davi Vieira";
 		String numeroConta = "01";
 		
+
 		
 		do{
-		switch(menuInicial()){
+		switch(menuInicial(leia)){
+
 		
 			case '1': 
-			{		
+			{						
+				System.out.println("DIGITE O DIA DE HOJE: ");
+				aniversarioConfirmacao = leia.nextInt();
 				for(int i=0; i<3; i++){
 					
 					System.out.println("MOVIMENTAÇÃO "+ (i+1)+ " - [D]DÉBITO OU [C] CRÉDITO");
@@ -36,25 +42,54 @@ public class contaPoupanca {
 							if(i == 2){
 								movimentos[i]= (-debito);
 								saldo += movimentos[i];
-								System.out.println("SALDO ATUAL: R$ "+saldo);
-								System.out.println("DESEJA RETORNAR AO MENU INICIAL? S/N");
-								continuaMenu = leia.next().toUpperCase().charAt(0);
+								
+									if(aniversarioConta == aniversarioConfirmacao && saldo > 0){
+										saldo = saldo * 1.005;
+										System.out.println("PARABÉNS HOJE É O ANIVERSÁRIO DA SUA CONTA! O SEU SALDO RENDEU!");
+										System.out.printf("SALDO ATUAL: R$ %.2f",saldo);
+										pula();
+										System.out.println("DESEJA RETORNAR AO MENU INICIAL? S/N");
+										continuaMenu = leia.next().toUpperCase().charAt(0);
 
-							}else{
+									}else if(aniversarioConta == aniversarioConfirmacao && saldo == 0){
+									
+										System.out.println("PARABÉNS HOJE É O ANIVERSÁRIO DA SUA CONTA!");
+										System.out.println("INFELIZMENTE NÃO HOUVE RENDIMENTO POIS SEU SALDO É R$0,00");
+										System.out.println("DESEJA RETORNAR AO MENU INICIAL? S/N");
+										continuaMenu = leia.next().toUpperCase().charAt(0);
+									
+									}else {
+										pula();
+										System.out.printf("SALDO ATUAL: R$ %.2f",saldo);
+										System.out.println("DESEJA RETORNAR AO MENU INICIAL? S/N");
+										continuaMenu = leia.next().toUpperCase().charAt(0);
+									}
+							}
+														
+							else{
 								movimentos[i]= (-debito);
 								saldo += movimentos[i];
-								System.out.println("SALDO ATUAL: R$ "+saldo);
+								pula();
+								System.out.printf("SALDO ATUAL: R$ %.2f",saldo);
 								System.out.println("DESEJA CONTINUAR? S/N");
 								continuaLoop = leia.next().toUpperCase().charAt(0);
 									if(continuaLoop == 'N'){
 										break;	
 									}
-							}
+								}
 						}else if(saldo < debito) {
 							pula();
 							System.out.println("SALDO INSULFICIENTE");
 							System.out.println("DESEJA CONTINUAR? S/N");
 							continuaLoop = leia.next().toUpperCase().charAt(0);
+								if(i == 2) {
+									pula();
+									System.out.println("SALDO INSULFICIENTE");
+									System.out.println("DESEJA CONTINUAR? S/N");
+									continuaLoop = leia.next().toUpperCase().charAt(0);
+									System.out.println("DESEJA RETORNAR AO MENU INICIAL? S/N");
+									continuaMenu = leia.next().toUpperCase().charAt(0);
+								}
 								if(continuaLoop == 'N'){
 									break;
 								}
@@ -65,30 +100,47 @@ public class contaPoupanca {
 								credito = leia.nextDouble();
 								movimentos[i] = credito;
 								saldo += movimentos[i]; 
-								System.out.println("SALDO ATUAL: R$ "+saldo);
-								System.out.println("MOVIMENTAÇÕES ENCERRADAS.");
-								linha();
-								System.out.println("DESEJA RETORNAR AO MENU INICIAL? S/N");
-								continuaMenu = leia.next().toUpperCase().charAt(0);
+									if(aniversarioConta == aniversarioConfirmacao && saldo > 0){	
+										saldo = saldo * 1.005;
+										System.out.println("PARABÉNS HOJE É O ANIVERSÁRIO DA SUA CONTA! O SEU SALDO RENDEU!");
+										System.out.printf("SALDO ATUAL: R$ %.2f",saldo);
+										pula();
+										System.out.println("DESEJA RETORNAR AO MENU INICIAL? S/N");
+										continuaMenu = leia.next().toUpperCase().charAt(0);	
+									}else if(aniversarioConta == aniversarioConfirmacao && saldo == 0){
+										System.out.println("PARABÉNS HOJE É O ANIVERSÁRIO DA SUA CONTA!");
+										System.out.println("INFELIZMENTE NÃO HOUVE RENDIMENTO POIS SEU SALDO É R$0,00");
+										System.out.println("DESEJA RETORNAR AO MENU INICIAL? S/N");
+										continuaMenu = leia.next().toUpperCase().charAt(0);
+									}else {		
+										System.out.printf("SALDO ATUAL: R$ %.2f",saldo);
+										pula();
+										System.out.println("MOVIMENTAÇÕES ENCERRADAS.");
+										linha();
+										System.out.println("DESEJA RETORNAR AO MENU INICIAL? S/N");
+										continuaMenu = leia.next().toUpperCase().charAt(0);
+									}
 
+							}else {
+								System.out.println("DIGITE O VALOR DO CRÉDITO: R$");
+								credito = leia.nextDouble();
+								movimentos[i] = credito;
+								saldo += movimentos[i]; 
+								System.out.println("DESEJA CONTINUAR? S/N");
+								continuaLoop = leia.next().toUpperCase().charAt(0);
+									if(continuaLoop == 'N'){
+										break;								
+									}
 							}
-						System.out.println("DIGITE O VALOR DO CRÉDITO: R$");
-						credito = leia.nextDouble();
-						movimentos[i] = credito;
-						saldo += movimentos[i]; 
-						System.out.println("DESEJA CONTINUAR? S/N");
-						continuaLoop = leia.next().toUpperCase().charAt(0);
-							if(continuaLoop == 'N'){
-								break;								
-							}
-					}									
+					  }									
 				}
 			break;
 			}
 
 			case '2' : System.out.println("NOME DO CLIENTE: "+ nomeClientes);
 					   System.out.println("NÚMERO DA CONTA: "+ numeroConta);
-				       System.out.println("SALTO ATUAL: "+ saldo);	
+				       System.out.printf("SALTO ATUAL: %.2f", saldo);	
+					   pula();
 				       System.out.println("DESEJA VOLTAR AO MENU INICIAL? S/N");
 				       continuaMenu = leia.next().toUpperCase().charAt(0);
 				       break;
@@ -98,6 +150,8 @@ public class contaPoupanca {
 			
 			}
 		}while(continuaMenu == 'S' ||  continuaLoop == 'N');
+		
+		leia.close();
 		
 		}
 
@@ -111,9 +165,7 @@ public class contaPoupanca {
 			System.out.println();
 		}
 		
-		public static char menuInicial() {
-			
-			Scanner ler = new Scanner(System.in);
+		public static char menuInicial(Scanner leia) {
 			
 			linha();
 			pula();
@@ -121,7 +173,7 @@ public class contaPoupanca {
 			System.out.println("[2] - SALDO");
 			System.out.println("[3] - SAIR");
 			linha();			
-			char resposta = ler.next().charAt(0);
+			char resposta = leia.next().charAt(0);
 			
 			return resposta;
 		}
